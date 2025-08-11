d809933478b09850d9f07258be517ceeb3099946
BijanProgrammer <bijaneisapour@gmail.com>
2025-08-11 06:22:21 +0330

    [ADD] logic

---
+++ src/scripts/data.js
@@ -0,0 +1,19 @@
+import { Book } from "./book.js";
+
+export const initialBooks = [
+  new Book(
+    "Frontend Roadmap",
+    "BijanProgrammer",
+    "The ultimate essential roadmap to become a frontend developer.",
+  ),
+  new Book(
+    "Refactoring UI",
+    "Adam Wathan; Steve Schoger",
+    "Takes everything we know about design and bundles it into one comprehensive package.",
+  ),
+  new Book(
+    "Clean Code",
+    "Robert C. Martin",
+    "A Handbook of Agile Software Craftsmanship.",
+  ),
+];
