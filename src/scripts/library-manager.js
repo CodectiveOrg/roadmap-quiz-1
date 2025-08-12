113ce254b0c126c2199136e5b34905e996178307
ZahraZare <zahrazare3099@gmail.com>
2025-08-12 11:08:43 +0330

    debug

--- src/scripts/library-manager.js
+++ src/scripts/library-manager.js
@@ -3,14 +3,23 @@ export class LibraryManager {
   constructor(books) {
     this.#books = books.map((book) => book.clone());
   }
+
   get books() {
     return this.#books;
   }
+
   addBook(book) {
     this.#books.push(book);
   }
-  editBook(row, book) {}
-  removeBook(row) {}
+
+  editBook(row, book) {
+    this.#books[row - 1] = book;
+  }
+
+  removeBook(row) {
+    this.#books.splice(row - 1, 1);
+  }
+
   clone() {
     return new LibraryManager(this.#books);
   }
